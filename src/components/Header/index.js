0576199f6de08659e37d6b62ba7eb0614e10eabf
Matheus Dario <dario.matheus97@gmail.com>
2023-10-23 01:12:41 -0300

    Add component logout user

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { FaHome, FaUserAlt, FaSignInAlt } from 'react-icons/fa';
+import {
+  FaHome, FaUserAlt, FaSignInAlt, FaCircle, FaPowerOff,
+} from 'react-icons/fa';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { useSelector, useDispatch } from 'react-redux';
 
+import * as actions from '../../store/modules/auth/actions';
+import history from '../../services/history';
 import { Nav } from './style';
 
 export default function Header() {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(actions.loginFailure());
+    history.push('/');
+  };
+
   return (
     <Nav>
       <Link to="/">
@@ -17,11 +31,21 @@ export default function Header() {
         <FaUserAlt size={22} />
         {' '}
       </Link>
-      <Link to="/login">
-        {' '}
-        <FaSignInAlt size={24} />
-        {' '}
-      </Link>
+      {isLoggedIn ? (
+        <Link onClick={handleLogout} to="/logout">
+          {' '}
+          <FaPowerOff size={24} />
+          {' '}
+        </Link>
+      ) : (
+        <Link to="/login">
+          { ' ' }
+          <FaSignInAlt size={24} />
+          {' '}
+        </Link>
+      )}
+
+      {isLoggedIn && <FaCircle size={22} color="#66ff33" />}
     </Nav>
   );
 }
