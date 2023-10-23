8951541bc5465837fdd95772ffa4333064041d3c
Matheus Dario <dario.matheus97@gmail.com>
2023-10-23 00:51:25 -0300

    Add component update user

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,19 +1,31 @@
 import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import { isEmail } from 'validator';
-import { get } from 'lodash';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Form, Titulo } from './styled';
-import axios from '../../services/axios';
-import history from '../../services/history';
 import Loading from '../../components/Loading';
+import * as actions from '../../store/modules/auth/actions';
 
 export default function Register() {
+  const dispatch = useDispatch();
+
+  const id = useSelector((state) => state.auth.user.id);
+  const nomeStored = useSelector((state) => state.auth.user.nome);
+  const emailStored = useSelector((state) => state.auth.user.email);
+  const isLoading = useSelector((state) => state.auth.isLoading);
+
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+
+  React.useEffect(() => {
+    if (!id) return;
+
+    setNome(nomeStored);
+    setEmail(emailStored);
+  }, [id, nomeStored, emailStored]);
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -27,35 +39,22 @@ export default function Register() {
       formErrors = true;
       toast.error('O E-mail informado deve ser um E-mail válido');
     }
-    if (password.length < 6 || password.length > 50) {
+    if (!id && (password.length < 6 || password.length > 50)) {
       formErrors = true;
-      toast.error('Senha deve ter entre 6 e 50 caracteres');
+      toast.error('Senha digitada deve ter entre 6 e 50 caracteres');
     }
 
     if (formErrors) return;
 
-    setIsLoading(true);
-    try {
-      await axios.post('/users/', {
-        nome, password, email,
-      });
-
-      toast.success('Sua conta foi criada com Sucesso!');
-      setIsLoading(false);
-
-      history.push('/login');
-    } catch (err) {
-      const errors = get(err, 'response.data.errors', []);
-
-      errors.map((error) => toast.error(error));
-      setIsLoading(false);
-    }
+    dispatch(actions.registerRequest({
+      nome, email, password, id,
+    }));
   }
 
   return (
     <Container>
       <Loading isLoading={isLoading} />
-      <Titulo>Crie sua conta</Titulo>
+      <Titulo>{id ? 'Editar Seus Dados' : 'Crie sua conta'}</Titulo>
       <Form onSubmit={(e) => handleSubmit(e)}>
         <label htmlFor="nome">
           <span>Nome:</span>
@@ -85,7 +84,7 @@ export default function Register() {
           />
         </label>
 
-        <button type="submit">Criar Minha Conta</button>
+        <button type="submit">{id ? 'Salvar Dados Editado' : 'Crie Sua Conta'}</button>
       </Form>
     </Container>
   );
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -8,11 +8,16 @@ import * as types from '../types';
 import axios from '../../../services/axios';
 import history from '../../../services/history';
 
+// eslint-disable-next-line consistent-return
 function* loginRequest({ payload }) {
   try {
     const response = yield call(axios.post, '/tokens', payload);
-    yield put(actions.loginSuccess({ ...response.data }));
 
+    if (response.data.errors) {
+      toast.error('E-mail ou Senha é inválido');
+      return yield put(actions.loginFailure());
+    }
+    yield put(actions.loginSuccess({ ...response.data }));
     toast.success('Você fez login');
 
     axios.defaults.headers.Authorization = `Bearer ${response.data.token}`;
@@ -20,7 +25,6 @@ function* loginRequest({ payload }) {
     history.push(payload.prevPath);
   } catch (e) {
     toast.error('Usuário ou senha inválidos.');
-
     yield put(actions.loginFailure());
   }
 }
@@ -30,7 +34,53 @@ function persistRehydrate({ payload }) {
   if (!token) return;
   axios.defaults.headers.Authorization = `Bearer ${token}`;
 }
+
+// eslint-disable-next-line consistent-return
+function* registerRequest({ payload }) {
+  const {
+    id, nome, email, password,
+  } = payload;
+  try {
+    if (id) {
+      yield call(axios.put, '/users', {
+        email,
+        nome,
+        password: password || undefined,
+      });
+      toast.success('Sua conta foi Alterada com Sucesso!');
+      yield put(actions.registerUpdatedSuccess({ nome, email, password }));
+    } else {
+      yield call(axios.post, '/users', {
+        email,
+        nome,
+        password: password || undefined,
+      });
+      toast.success('Sua conta foi Criada com Sucesso!');
+      yield put(actions.registerCreatedSuccess({ nome, email, password }));
+      history.push('/login');
+    }
+  } catch (e) {
+    const errors = get(e, 'response.data.errors', []);
+    const status = get(e, 'response.status', 0);
+
+    if (status === 401) {
+      toast.warn('Você precisa fazer login novamente.');
+      yield put(actions.loginFailure());
+      return history.push('/login');
+    }
+
+    if (errors.length > 0) {
+      errors.map((error) => toast.error(error));
+    } else {
+      toast.error('Erro desconhecido');
+    }
+
+    return yield put(actions.registerFailure());
+  }
+}
+
 export default all([
   takeLatest(types.LOGIN_REQUEST, loginRequest),
   takeLatest(types.PERSIST_REHYDRATE, persistRehydrate),
+  takeLatest(types.REGISTER_REQUEST, registerRequest),
 ]);
